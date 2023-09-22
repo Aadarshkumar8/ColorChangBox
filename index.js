001d7d2b7f05c52096264c78b1cd7dafd7a19416
Aadarsh Kumar <ak0001734@gmail.com>
2023-09-22 10:23:39 +0530

    This is color changing box

---
+++ index.js
@@ -0,0 +1,63 @@
+// let redBox=document.getElementById("red")
+// let blueBox=document.getElementById("blue")
+// let greenBox=document.getElementsByClassName("green")
+// redBox.addEventListener('click',()=>{
+//     redBox.style.color='white;'
+//     redBox.style.backgroundColor='red'
+// })
+// let redBox=document.getElementById('red');
+// let blueBox=document.getElementById('blue');
+// let greenBox=document.getElementsById('green');
+// let blackBox=document.getElementById('black');
+// let yellowBox=document.getElementById('yellow');
+// let purpleBox=document.getElementById('purple');
+// redBox.addEventListener('click', ()=>{
+//  redBox.style.color='white';
+// redBox.style.background='red';
+// });
+
+// blueBox.addEventListener('click', ()=>{
+//  blueBox.style.color='white';
+//  blueBox.style.background='blue';
+// });
+// greenBox[0].addEventListener('click', ()=>{
+//  greenBox[0].style.color='white';
+//  greenBox[0].style.background='green';
+// });
+// yellowBox[1].addEventListener('click', ()=>{
+//     yellowBox[1].style.color='white';
+//     yellowBox[1].style.backgroundColor="yellow"
+// })
+
+let redBox=document.getElementById('red')
+redBox.addEventListener('click',()=>{
+    redBox.style.color='white'
+    redBox.style.backgroundColor='red'
+})
+let yellowBox=document.getElementById('yellow')
+yellowBox.addEventListener('click',()=>{
+    yellowBox.style.color='black'
+    yellowBox.style.backgroundColor='yellow'
+})
+let blackBox=document.getElementById('black')
+blackBox.addEventListener('click', ()=>{
+    blackBox.style.color='white'
+    blackBox.style.backgroundColor='black'
+})
+let greenBox=document.getElementById('green')
+greenBox.addEventListener('click',()=>{
+    greenBox.style.color='white'
+    greenBox.style.backgroundColor='green'
+})
+
+let purpleBox=document.getElementById('purple')
+purpleBox.addEventListener('click',()=>{
+    purpleBox.style.color='white'
+    purpleBox.style.backgroundColor='purple'
+})
+let blueBox=document.getElementById('blue')
+blueBox.addEventListener('click',()=>{
+    blueBox.style.color='white'
+    blueBox.style.backgroundColor='blue'
+})
+
